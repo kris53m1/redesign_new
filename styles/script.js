2497e8a165385639800734a8478a1550b5f02d3d
Eryk Munk <erykmunk@MacBook-Pro-tilhrende-Eryk.local>
2020-12-16 12:53:03 +0100

    billede optimering

--- styles/script.js
+++ styles/script.js
@@ -74,25 +74,25 @@ function myVolumeBar() {
 
 // javaScript til burger menu
 
-window.addEventListener("load", sidenVises);
-
-function sidenVises() {
-    console.log("sidenVises");
-
-    document.querySelector("#menuknap").addEventListener("click", toggleMenu)
-
-}
-
-function toggleMenu() {
-    console.log("toggleMenu");
-
-    document.querySelector("#menu").classList.toggle("hidden");
-
-    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
-
-    if (erSkjult == true) {
-        document.querySelector("#menuknap").textContent = "☰";
-    } else {
-        document.querySelector("#menuknap").textContent = "✖";
-    }
-}
+//window.addEventListener("load", sidenVises);
+//
+//function sidenVises() {
+//    console.log("sidenVises");
+//
+//    document.querySelector("#menuknap").addEventListener("click", toggleMenu)
+//
+//}
+//
+//function toggleMenu() {
+//    console.log("toggleMenu");
+//
+//    document.querySelector("#menu").classList.toggle("hidden");
+//
+//    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+//
+//    if (erSkjult == true) {
+//        document.querySelector("#menuknap").textContent = "☰";
+//    } else {
+//        document.querySelector("#menuknap").textContent = "✖";
+//    }
+//}
